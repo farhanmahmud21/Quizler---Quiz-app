49f268e64c47e5f241b96233ec891a50a466b3be
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2024-07-04 17:59:29 +0600

    question changed

--- lib/quizBrain.dart
+++ lib/quizBrain.dart
@@ -8,7 +8,7 @@ class QuizBrain {
     Question('A slug\'s blood is green.', true),
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('Approddximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
     Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
     Question('It is illegal to pee in the Ocean in Portugal.', true),
